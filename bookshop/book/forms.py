4e2cc3b2f63ceb7c83a57bb9f4ef7c74e9bca132
Amir <69367070+mrnulo@users.noreply.github.com>
2023-08-31 10:21:46 +0330

    Delete bookshop directory

--- bookshop/book/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Book
-
-class BookForm(forms.ModelForm):
-    image = forms.ImageField(required=False)
-
-    class Meta:
-        model = Book
-        fields = ('title', 'author', 'publication_date', 'image')
